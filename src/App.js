accc79d4be6383ebc206d0c182cdb458874cfcdf
Daniel Roberts <Dan@howards-air.lan>
2020-05-02 15:09:56 +0100

    wiped and imported new d3-react environment

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import ChartWrapper from "./ChartWrapper";
+
+const App = () => {
+  const [covidData, setCovidData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://pomber.github.io/covid19/timeseries.json")
+      .then((res) => {
+        return res.data;
+      })
+      .then((data) => {
+        setIsLoading(false);
+        console.log(data);
+        setCovidData(data);
+      })
+      .catch((error) => {
+        setIsLoading(true);
+        console.log(error);
+      });
+  }, []);
+
+  const display = isLoading ? covidData.Ireland : null;
+  console.log(display)
+  return (
+    <div>
+      <Navbar bg="light">
+        <Navbar.Brand>Covid Viz</Navbar.Brand>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col md={6} xs={12}>
+            <ChartWrapper data = {display}/>
+          </Col>
+          <Col md={6} xs={12}></Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default App;
